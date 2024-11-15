7d927920369ef54f64befbf67cb63cd606f4b8c3
Elizalde Baguinon <mizmerize@yahoo.com>
2024-11-15 23:31:46 +0800

    Initial commit

---
+++ crypt.go
@@ -0,0 +1,68 @@
+// Package crypt is a package for handling easy crypt handling
+//
+//	Author: Elizalde G. Baguinon
+//	Created: October 17, 2019
+package crypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	b64 "encoding/base64"
+	"errors"
+	"io"
+)
+
+// Encrypt encrypts a string using AES
+func Encrypt(plainText []byte, key []byte) ([]byte, error) {
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return nil, err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	return gcm.Seal(nonce, nonce, plainText, nil), nil
+}
+
+// Decrypt decrypts a string using AES
+func Decrypt(cipherText []byte, key []byte) ([]byte, error) {
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return nil, err
+	}
+	nonceSize := gcm.NonceSize()
+	if len(cipherText) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+	nonce, cipherText := cipherText[:nonceSize], cipherText[nonceSize:]
+	return gcm.Open(nil, nonce, cipherText, nil)
+}
+
+// EncodeText encodes plain text with a key and returns an encrypted and base64-encoded string
+func EncodeText(plainText string, key []byte) string {
+	if plainText == "" {
+		return plainText
+	}
+	enc, _ := Encrypt([]byte(plainText), key)
+	return b64.RawURLEncoding.WithPadding(b64.NoPadding).EncodeToString(enc)
+}
+
+// DecodeText decodes an encypted base64-encoded text with a key and returns a decrypted string
+func DecodeText(encoded string, key []byte) string {
+	if encoded == "" {
+		return encoded
+	}
+	benc, _ := b64.RawURLEncoding.WithPadding(b64.NoPadding).DecodeString(encoded)
+	dec, _ := Decrypt(benc, key)
+	return string(dec)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/stdutil/crypt
+
+go 1.23.2
